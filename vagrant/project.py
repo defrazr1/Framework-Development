a0d431d5ee4b199c71f2761dd645b776ecabfc5a
Demitrio Frazier <dfdefrazr@gmail.com>
2017-08-07 07:11:38 -0700

    addding JSON endpoint for GET request of menu items

--- vagrant/project.py
+++ vagrant/project.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template, request, redirect, url_for, flash
+from flask import Flask , render_template, request, redirect, url_for, flash, jsonify
 app = Flask(__name__)
 
 from sqlalchemy import create_engine
@@ -11,7 +11,14 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-# @app.route('/')
+#Making an API Endpoint (GET Request)
+@app.route('/restaurants/<int:restaurant_id>/menu/JSON')
+def restaurantMenuJSON(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    items = session.query(MenuItem).filter_by(
+        restaurant_id=restaurant_id).all()
+    return jsonify(MenuItems=[i.serialize for i in items])
+
 @app.route('/restaurants/<int:restaurant_id>/')
 def restaurantMenu(restaurant_id):
     restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()

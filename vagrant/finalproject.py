fb40f2152bb75176c099d077f4fac776e3d7c067
Demitrio Frazier <dfdefrazr@gmail.com>
2017-08-10 07:22:51 -0700

    updated webserver with CREATE and UPDATE functionality for menus

--- vagrant/finalproject.py
+++ vagrant/finalproject.py
@@ -25,7 +25,7 @@ def newRestaurant():
         addRestaurant = Restaurant(name = request.form['name'])
         session.add(addRestaurant)
         session.commit()
-        flash("New restaurant, %s, created!") % (addRestaurant.name)
+        flash(("New restaurant, %s, created!") % (addRestaurant.name))
         return redirect(url_for('showRestaurants'))
     else:
         return render_template('newRestaurant.html')
@@ -37,10 +37,11 @@ def editRestaurant(restaurant_id):
     if request.method == 'POST':
         if request.form['name']:
             editedRestaurant.name = request.form['name']
-            session.add(editedRestaurant)
-            session.commit()
-            flash(("%s has been renamed to %s") % (editedRestaurantOld, editedRestaurant.name))
-            return redirect(url_for('showRestaurants'))
+
+        session.add(editedRestaurant)
+        session.commit()
+        flash(("%s has been renamed to %s") % (editedRestaurantOld, editedRestaurant.name))
+        return redirect(url_for('showRestaurants'))
     else:
         return render_template('editRestaurant.html', restaurant = editedRestaurant)
 
@@ -67,11 +68,43 @@ def showMenu(restaurant_id):
 
 @app.route('/restaurants/<int:restaurant_id>/menu/new/', methods = ['GET', 'POST'])
 def newMenuItem(restaurant_id):
-    return render_template('newMenuItem.html', restaurant = restaurant)
+    if request.method == 'POST':
+        if request.form['name']:
+            itemName = request.form['name']
+
+        if request.form['description']:
+            itemDescription = request.form['description']
+
+        if request.form['price']:
+            itemPrice = (("$%s") % (request.form['price']))
+
+        newItem = MenuItem(
+            name = itemName,
+            price = itemPrice,
+            description = itemDescription,
+            restaurant_id = restaurant_id)
+
+        session.add(newItem)
+        session.commit()
+        flash(("New menu item, %s, created!") % (newItem.name))
+        return redirect(url_for('showMenu', restaurant_id = restaurant_id))
+    else:
+        return render_template('newMenuItem.html', restaurant_id = restaurant_id)
 
 @app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/edit/', methods = ['GET', 'POST'])
 def editMenuItem(restaurant_id, menu_id):
-    return render_template('editMenuItem.html', restaurant = restaurant, menu_id = menu_id, item = item)
+    editedItem = session.query(MenuItem).filter_by(id = menu_id).one()
+    editedItemOldName = editedItem.name
+    if request.method == 'POST':
+        if request.form['name']:
+            editedItem.name = request.form['name']
+        session.add(editedItem)
+        session.commit()
+        flash(("%s has been renamed to %s") % (editedItemOldName, editedItem.name))
+        return redirect(url_for('showMenu', restaurant_id = restaurant_id))
+    else:
+        return render_template('editMenuItem.html', restaurant_id = restaurant_id,
+                    menu_id = menu_id, item = editedItem)
 
 @app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/delete/', methods = ['GET', 'POST'])
 def deleteMenuItem(restaurant_id, menu_id):
